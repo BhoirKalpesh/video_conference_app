5cf999682f11c10daf2451d6cd4bc48fcb22e866
KalpeshBhoir <kalpeshbhoir2000@gmail.com>
2024-04-20 01:57:52 +0530

    background-image

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -12,14 +12,13 @@ interface HomeCardProps {
 
 const HomeCard = ({className,img,title,description,handleClick}:HomeCardProps) => {
   return (
-    <div className={cn('px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer',className)}
+    <div className={cn('px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] transform hover:scale-[1.02] active:scale-100 transition-transform duration-300 cursor-pointer',className)}
     onClick={handleClick}
     >
         <div className='flex-center glassmorphism size-12 rounded-[10px]'>
         <Image src={img}
         alt='meeting' width={27} height={27}/>
         </div>
-
         <div className="flex flex-col gap-2 ">
         <h1 className='text-2xl font-bold'>{title}</h1>
         <p className='text-lg font-normal'>{description}</p>
